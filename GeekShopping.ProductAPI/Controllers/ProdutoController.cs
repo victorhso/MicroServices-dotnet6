27d5c3954cbd64391d9c05d36932aa181efcb4d1
Victor Oliveira <victor.souza.41@hotmail.com>
2022-03-17 17:15:36 -0300

    Criado Controllers com métodos GET

---
+++ GeekShopping.ProductAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,40 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private IProdutoRepository _repository;
+
+        public ProdutoController(IProdutoRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProdutoVO>> BuscarPorId(long id)
+        {
+            var produto = await _repository.BuscarPorId(id);
+
+            if(produto == null)
+                return NotFound();
+
+            return Ok(produto);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProdutoVO>>> ListarProdutos()
+        {
+            var produto = await _repository.Listarprodutos();
+
+            if (produto == null)
+                return NotFound();
+
+            return Ok(produto);
+        }
+    }
+}
